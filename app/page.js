814a224d62c76dd53b7e63a8940f62685dbbc41c
Archna29 <archna1129@gmail.com>
2024-06-30 10:48:36 +0530

    added shadcn-ui button; fixed build

---
+++ app/page.js
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button"
+export default function Home() {
+  return (
+    
+        <div>
+Hellow there
+<Button>click</Button>
+
+        </div>
+
+  );
+}
